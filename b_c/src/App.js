3ebfebb19e2963657b59e04f28f353c597bea7e1
junsung202067 <junsung2264@gmail.com>
2024-06-23 11:30:10 +0900

    2024/06/23
    -파일 받기 및 확인

--- b_c/src/App.js
+++ b_c/src/App.js
@@ -3,6 +3,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p> 심심하잖아~ </p>
+        <p> 안녕하세요 </p>
+        <p> 심심하잖아~ </p>
       </header>
     </div>
   );
