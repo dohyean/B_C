61916f45cea5db9a37564e5d48264aa26625f692
junsung202067 <junsung2264@gmail.com>
2024-06-24 17:46:03 +0900

    2024/06/24
    지금 다른 디렉토리에서 css를 불러오는게 안되서 답답하다. 내가 어디서 놓치는건지 참

--- b_c/src/App.js
+++ b_c/src/App.js
@@ -1,12 +1,47 @@
-function App() {
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+/* import "./Style/homepage.css"; 이거 왜 안되냐*/
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <p> 심심하잖아~ </p>
-        <p> 안녕하세요 </p>
-        <p> 심심하잖아~ </p>
+        <h1>ABCD</h1>
+        <Link to="/login">
+          <button className="login-button">로그인</button>
+        </Link>
       </header>
+      <div className="App-content">
+        <div className="description">
+          <p>페이지에 대한 설명</p>
+        </div>
+        <div className="image-section">
+          <img src={process.env.PUBLIC_URL + "/image.jpg"} alt="Description" />
+        </div>
+      </div>
     </div>
+  );
+}
+
+function Login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>로그인 페이지</h1>
+      </header>
+      <div className="App-content">
+        <p>여기에 로그인 폼을 추가하세요.</p>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
