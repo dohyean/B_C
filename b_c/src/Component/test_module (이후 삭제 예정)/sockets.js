a59f834af59c96f8f07d0e74d2db7ab6baa3fe06
dohyean <65577625+dohyean@users.noreply.github.com>
2024-07-05 23:33:41 +0900

    2024-07-05
    - sql 연동 성공

--- b_c/src/Component/test_module (이후 삭제 예정)/sockets.js
+++ b_c/src/Component/test_module (이후 삭제 예정)/sockets.js
@@ -1,9 +1,9 @@
 import io from "socket.io-client";
-const socket = io("http://localhost:8080", { transports: ["websocket"] });
+// const socket = io("http://localhost:3001", { transports: ["websocket"] });
+const socket = io("http://localhost:3001");
 
 const UserData = require("./UserData.js");
 
 export const UserData_Save = (formData) => {
-  alert("test");
   UserData.Send_User_Data(socket, formData);
 };
