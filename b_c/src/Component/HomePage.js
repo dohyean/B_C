0c3294358dacc966352216c7b4fb0302682913a2
junsung202067 <junsung2264@gmail.com>
2024-07-06 17:35:46 +0900

    Revert "2024/07/05 (junsung)"
    This reverts commit 64ffc2391da01a9dd69dc770e4cbffccca579a59.

--- b_c/src/Component/HomePage.js
+++ b_c/src/Component/HomePage.js
@@ -1,9 +1,5 @@
 import "../Style/HomePage.css";
 import Menubar from "./Menubar";
-import junsungImage from "../images/junsung.jpg";
-import maplekingImage from "../images/mapleking.jpg";
-import summerImage from "../images/summer.jpg";
-import summer1Image from "../images/summer1.jpg";
 
 function HomePage() {
   return (
@@ -11,22 +7,10 @@ function HomePage() {
       <Menubar></Menubar>
       <div className="App-content-Home">
         <div className="description">
-          <p>made by 전도띠</p>
-          <p>made by 권준성</p>
+          <p>페이지에 대한 설명</p>
         </div>
         <div className="image-section">
-          <div className="image-quarter">
-            <img src={junsungImage} alt="사진 1" />
-          </div>
-          <div className="image-quarter">
-            <img src={maplekingImage} alt="사진 2" />
-          </div>
-          <div className="image-quarter">
-            <img src={summerImage} alt="사진 3" />
-          </div>
-          <div className="image-quarter">
-            <img src={summer1Image} alt="사진 4" />
-          </div>
+          <img alt="프로젝트 관련 사진 2~3장adsfkldjlkfjsdfjsdfk" />
         </div>
       </div>
     </div>
